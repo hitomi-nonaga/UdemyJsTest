177011ebffd2ed83f4309ed4a2b2bbac62b606f5
hitomi-nonaga <39607832+hitomi-nonaga@users.noreply.github.com>
2021-04-09 18:22:21 +0900

    分割代入のとこまでやった

--- src/index.js
+++ src/index.js
@@ -36,7 +36,67 @@
 // console.log(val4);
 
 // constで定義した配列はプロパティの変更が可能
-const val5 = ["dog", "cat"];
-val5[0] = "bird";
-val5.push("monkey");
-console.log(val5);
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+// const name = "test";
+// const age = "29";
+// // 「私の名前はtestです。年齢は29です。」
+// // 従来
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
+// // テンプレート文字列
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+// 従来
+// function func1(str) {
+//   return str;
+// }
+const func1 = function (str) {
+  return str;
+};
+console.log(func1("func1です。"));
+// アロー関数
+const func2 = (str) => {
+  return str;
+};
+console.log(func2("func2です。"));
+
+const func3 = (num1, num2) => num1 + num2;
+console.log(func3(10, 20));
+
+/**
+ * 分割代入
+ */
+// const myProfile = {
+//   name: "テスト",
+//   age: 28
+// };
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+// 配列の場合
+const myProfile = ["test", "29"];
+const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
+console.log(message3);
+
+const [name, age] = myProfile;
+const message4 = `名前は${name}です。年齢は${age}です。`;
+console.log(message4);
+
+/**
+ * デフォルト値、引数など
+ */
