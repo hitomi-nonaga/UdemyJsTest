642a421a2a2ef4c5275e1a3d28a87fa0f44013ed
hitomi-nonaga <39607832+hitomi-nonaga@users.noreply.github.com>
2021-04-09 17:19:58 +0900

    変数宣言

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
-import "./styles.css";
+/**
+ * const,letなどの変数宣言
+ */
 
-console.log("aa");
+// var val1 = "var変数";
+// console.log(val1);
+// // var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+// // var変数は再宣言可能
+// var val1 = "var変数は再宣言可能";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// let val2 = "let変数";
+// console.log(val2);
+// // let変数は上書き可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+// // let変数は再宣言不可
+// let val2 = "let変数を再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+// const変数は上書き不可
+// val3 = "const変数を上書き";
+// // const変数は再宣言不可
+// let val3 = "const変数を再宣言";
+
+// constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "test",
+//   age: 29
+// };
+// val4.name = "jak";
+// val4.address = "hiroshima";
+// console.log(val4);
+
+// constで定義した配列はプロパティの変更が可能
+const val5 = ["dog", "cat"];
+val5[0] = "bird";
+val5.push("monkey");
+console.log(val5);
